9def471620864a65d5ef8b3ef34c15545abdb0ad
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-01-20 19:09:18 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+let admin;
+let name = "John";
+
+admin = name;
+
+alert(admin);
+
+let planetEarth = "Earth";
+let currenUserName = "John";
+
+const BIRTHDAY = "18.04.1982";
+
+let age = console.log(BIRTHDAY);
+
+let firtNumber = 100;
+let secondNumber = 50;
+
+let sum = firtNumber + secondNumber;
+
+console.log(sum);
+
+let x = (100 + 50) * 10;
+
+console.log(x);
+
+let a = 5;
+let b = 2;
+let c = a % b;
+
+console.log(c);
+
+let d = 5;
+d++;
+
+console.log(d);
+
+let e = 5;
+let f = e ** 2;
+
+console.log(f);
+
+let g = 100 + 50 * 3;
+
+console.log(g);
+
+let h = 10 / 2;
+
+alert(h);
+
+// string concatenation
+
+let i = 10; // this is a number
+let j = "20"; // this is a string( anything with a double quotation is a string )
+let k = i * j; // combining numbers with  numbers with double quotaions/string will work  excpet for addition
+
+console.log(k);
+
+let myNumber = 32;
+let myNumberToString = myNumber.toString(32);
+
+console.log(myNumber);
+console.log(myNumberToString);
+
+let l = 10;
+let m = 10;
+let n = l == m;
+console.log(n);
